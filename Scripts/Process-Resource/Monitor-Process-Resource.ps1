64c317dbacdee65ca0cec33f58fa36c3b64eb027
ShiqianTao <shtao@microsoft.com>
2023-03-02 14:55:34 +0800

    Add scripts

--- Scripts/Process-Resource/Monitor-Process-Resource.ps1
+++ Scripts/Process-Resource/Monitor-Process-Resource.ps1
@@ -1,5 +1,5 @@
 filter Timestamp { "$(Get-Date -Format o): $_" }
-$LogFilepath = ".\process-resource-" + $(Get-Date -f yyyyMMdd-HHmmss) + ".log"
+$LogFilepath = ".\log-process-resource-" + $(Get-Date -f yyyyMMdd-HHmmss) + ".log"
 $ProcessNumber = 20 # top 20 processes of memory usage
 $Interval = 60 # 60 second
 $MaxTime = 60 * 60  * 24 * 3 # 3 days

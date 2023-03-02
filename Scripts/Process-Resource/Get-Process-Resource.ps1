3486b04bf78209887ff9bbfa3aa8c24ff4fc38b9
ShiqianTao <shtao@microsoft.com>
2023-03-02 16:25:38 +0800

    Add scripts

--- Scripts/Process-Resource/Get-Process-Resource.ps1
+++ Scripts/Process-Resource/Get-Process-Resource.ps1
@@ -1,5 +1,5 @@
 filter Timestamp { "$(Get-Date -Format o): $_" }
 $LogFilepath = ".\log-process-resource-" + $(Get-Date -f yyyyMMdd-HHmmss) + ".log"
 Timestamp | Out-File -Append -Filepath $LogFilepath        
-Get-WmiObject Win32_OperatingSystem | Select-Object FreePhysicalMemory, FreeVirtualMemory | Out-File -Append -Filepath $LogFilepath
+systeminfo | find "Virtual Memory" | Out-File -Append -Filepath $LogFilepath
 Get-Process | Sort-Object WorkingSet -Descending | Select-Object | Out-File -Append -Filepath $LogFilepath
